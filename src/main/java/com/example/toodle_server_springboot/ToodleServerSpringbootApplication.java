613182a7cf3797513a8eb650bb353d8bcd125260
seonb2n <626ksb@gmail.com>
2023-01-03 20:24:12 +0900

    프로젝트 생성

---
+++ src/main/java/com/example/toodle_server_springboot/ToodleServerSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.example.toodle_server_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToodleServerSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToodleServerSpringbootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/toodle_server_springboot/ToodleServerSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.toodle_server_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToodleServerSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
