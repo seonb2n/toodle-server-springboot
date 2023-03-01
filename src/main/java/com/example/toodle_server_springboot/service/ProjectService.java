cea5880db89e541298175504ca331e569188aa80
seonb2n <626ksb@gmail.com>
2023-03-01 10:24:22 +0900

    - 테스트 코드를 통과하는 프로젝트 수정 서비스 로직 구현

--- src/main/java/com/example/toodle_server_springboot/service/ProjectService.java
+++ src/main/java/com/example/toodle_server_springboot/service/ProjectService.java
@@ -4,13 +4,17 @@ import com.example.toodle_server_springboot.domain.project.Project;
 import com.example.toodle_server_springboot.domain.project.task.Task;
 import com.example.toodle_server_springboot.domain.project.task.action.Action;
 import com.example.toodle_server_springboot.domain.user.UserAccount;
+import com.example.toodle_server_springboot.dto.project.ActionDto;
 import com.example.toodle_server_springboot.dto.project.ProjectDto;
+import com.example.toodle_server_springboot.dto.project.TaskDto;
 import com.example.toodle_server_springboot.repository.project.ActionRepository;
 import com.example.toodle_server_springboot.repository.project.ProjectRepository;
 import com.example.toodle_server_springboot.repository.project.TaskRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -27,26 +31,72 @@ public class ProjectService {
     public ProjectDto registerProject(UserAccount userAccount, ProjectDto projectDto) {
         Project project = projectDto.toEntity(userAccount);
         var savedProject = projectRepository.save(project);
-        projectDto.taskDtoSet().forEach(taskDto -> {
-            var task = Task.of(userAccount, savedProject, taskDto.content());
-            taskDto.actionDtoSet().forEach(actionDto -> {
-                var action = Action.of(userAccount, task, actionDto.content(), actionDto.dueDate(), actionDto.isDone());
-                actionRepository.save(action);
-                task.addAction(action);
-            });
-            taskRepository.save(task);
-            savedProject.addTask(task);
-        });
+        projectDto.taskDtoSet()
+                .forEach(taskDto -> registerTask(userAccount, savedProject, taskDto));
         return ProjectDto.from(savedProject);
     }
 
     /**
      * 기존에 존재하던 프로젝트를 수정하는 메서드
      */
-    public void updateProject() {}
+    @Transactional
+    public ProjectDto updateProject(UserAccount userAccount, ProjectDto projectDto) {
+        Project originalProject = projectRepository.findById(projectDto.projectId()).orElseThrow();
+        projectDto.taskDtoSet()
+                .forEach(taskDto -> {
+            Optional<Task> originalTaskOptional = taskRepository.findById(taskDto.taskId());
+            if (originalTaskOptional.isEmpty()) {
+                registerTask(userAccount, originalProject, taskDto);
+            } else {
+                Task savedTask = originalTaskOptional.get();
+                taskDto.actionDtoSet().forEach(actionDto -> {
+                    Optional<Action> originalActionOptional = actionRepository.findById(actionDto.actionId());
+                    if (originalActionOptional.isEmpty()) {
+                        registerAction(userAccount, savedTask, actionDto);
+                    } else {
+                        Action savedAction = originalActionOptional.get();
+                        savedAction.update(actionDto.content(), actionDto.dueDate(), actionDto.isDone());
+                    }
+                });
+                savedTask.update(taskDto.content());
+            }
+        });
+        originalProject.update(projectDto.projectName());
+        return ProjectDto.from(originalProject);
+    }
 
     /**
      * 기존에 존재하던 프로젝트를 삭제하는 메서드
      */
     public void deleteProject() {}
+
+    /**
+     * 태스크를 새로 등록하는 메서드
+     * @param userAccount
+     * @param project
+     * @param taskDto
+     * @return
+     */
+    public Task registerTask(UserAccount userAccount, Project project, TaskDto taskDto) {
+        var task = Task.of(userAccount, project, taskDto.content());
+        taskDto.actionDtoSet()
+                .forEach(actionDto -> registerAction(userAccount, task, actionDto));
+        taskRepository.save(task);
+        project.addTask(task);
+        return task;
+    }
+
+    /**
+     * 액션을 새로 등록하는 메서드
+     * @param userAccount
+     * @param task
+     * @param actionDto
+     * @return
+     */
+    public Action registerAction(UserAccount userAccount, Task task, ActionDto actionDto) {
+        Action action = Action.of(userAccount, task, actionDto.content(), actionDto.dueDate(), actionDto.isDone());
+        actionRepository.save(action);
+        task.addAction(action);
+        return action;
+    }
 }
