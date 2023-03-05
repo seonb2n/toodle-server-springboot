953d3acd1d2ddfd548e70d85932c116fa4ac9144
seonb2n <626ksb@gmail.com>
2023-03-05 10:13:46 +0900

    - FE 로 전달하는 포스트잇의 시간을 yyyy-MM-dd 로 변경

--- src/main/java/com/example/toodle_server_springboot/dto/PostItDto.java
+++ src/main/java/com/example/toodle_server_springboot/dto/PostItDto.java
@@ -3,22 +3,26 @@ package com.example.toodle_server_springboot.dto;
 import com.example.toodle_server_springboot.domain.postIt.PostIt;
 import com.example.toodle_server_springboot.domain.user.UserAccount;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.UUID;
 
 public record PostItDto(
         UUID postItId,
         String content,
-        LocalDateTime endTime,
+        String endTime,
         boolean isDone
 ) {
 
+    private static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+
     public static PostItDto of(String content, LocalDateTime endTime, boolean isDone) {
-        return new PostItDto(null, content, endTime, isDone);
+        return new PostItDto(null, content, endTime.format(DATE_TIME_FORMATTER), isDone);
     }
 
     public static PostItDto of(UUID postItId, String content, LocalDateTime endTime, boolean isDone) {
-        return new PostItDto(postItId, content, endTime, isDone);
+        return new PostItDto(postItId, content, endTime.format(DATE_TIME_FORMATTER), isDone);
     }
 
     public static PostItDto from(PostIt postIt) {
@@ -31,10 +35,11 @@ public record PostItDto(
     }
 
     public PostIt toEntity(UserAccount userAccount) {
+        LocalDate date = LocalDate.parse(endTime, DATE_TIME_FORMATTER);
         return PostIt.of(
                 content,
                 userAccount,
-                endTime
+                LocalDateTime.of(date, LocalDateTime.now().toLocalTime())
         );
     }
 
