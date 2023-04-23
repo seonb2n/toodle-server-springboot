55354d9130fcf0da589e4205ba0f80e67ea944c9
seonb2n <626ksb@gmail.com>
2023-04-23 20:46:13 +0900

    [TEST] - UserAccountController 의 기 가입된 이메일인지 확인하는 메서드 테스트 코드 추가

--- src/test/java/com/example/toodle_server_springboot/controller/UserAccountControllerTest.java
+++ src/test/java/com/example/toodle_server_springboot/controller/UserAccountControllerTest.java
@@ -4,6 +4,7 @@ import com.example.toodle_server_springboot.config.TestSecurityConfig;
 import com.example.toodle_server_springboot.config.security.JwtTokenUtil;
 import com.example.toodle_server_springboot.config.security.JwtUserDetailsService;
 import com.example.toodle_server_springboot.dto.request.UserAccountAuthenticateRequest;
+import com.example.toodle_server_springboot.service.UserAccountService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -15,10 +16,12 @@ import org.springframework.http.MediaType;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.BDDMockito.then;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -34,9 +37,11 @@ public class UserAccountControllerTest {
     @MockBean private JwtUserDetailsService jwtUserDetailsService;
     @MockBean private JwtTokenUtil jwtTokenUtil;
     @MockBean private AuthenticationManager authenticationManager;
+    private UserAccountService userAccountService;
 
-    UserAccountControllerTest(@Autowired MockMvc mvc) {
+    UserAccountControllerTest(@Autowired MockMvc mvc, @Autowired UserAccountService userAccountService) {
         this.mvc= mvc;
+        this.userAccountService = userAccountService;
     }
 
     @DisplayName("[view][POST] 로그인 api 컨트롤러 - 이메일, pw 없이 login api 호출시 400 에러가 발생한다.")
@@ -66,6 +71,44 @@ public class UserAccountControllerTest {
                 .andExpect(jsonPath("jwtToken").isNotEmpty());
         then(jwtUserDetailsService).should().loadUserByUsername(anyString());
         then(jwtTokenUtil).should().generateToken(any());
+    }
+
+    @DisplayName("[view][GET] 회원가입 api 컨트롤러 - 이미 가입된 적이 있는 Email 인지 검증한다 - 해당 이메일이 존재")
+    @Test
+    public void givenUserEmail_whenVerifyUserEmail_thenReturn200() throws Exception {
+        //given
+        String userEmail = "sbkim@naver.com";
+        given(userAccountService.checkEmail(userEmail)).willReturn(true);
+
+        //when
+        var response = mvc.perform(get("/api/v1/users/checkEmail")
+                .param("userEmail", userEmail)
+        )
+                .andExpect(status().isOk())
+                .andReturn();
+
+        //then
+        then(userAccountService).should().checkEmail(anyString());
+        assertEquals("true", response.getResponse().getContentAsString());
+    }
+
+    @DisplayName("[VIEW][GET] 회원가입 api 컨트롤러 - 이미 가입된 적이 있는 Email 인지 검증한다 - 해당 이메일이 존재하지 않음")
+    @Test
+    public void given_when_then() throws Exception {
+        //given
+        String userEmail = "sbkim@naver.com";
+        given(userAccountService.checkEmail(userEmail)).willReturn(false);
+
+        //when
+        var response = mvc.perform(get("/api/v1/users/checkEmail")
+                        .param("userEmail", userEmail)
+                )
+                .andExpect(status().isOk())
+                .andReturn();
+
+        //then
+        then(userAccountService).should().checkEmail(anyString());
+        assertEquals("false", response.getResponse().getContentAsString());
     }
 
 }
