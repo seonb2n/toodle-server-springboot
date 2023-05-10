c249a5f34b41a06ce6d07ec64ab90d3c14f84b79
seonb2n <626ksb@gmail.com>
2023-05-10 21:05:42 +0900

    [FEATURE]  - 세션 저장소로 Redis 를 사용하도록 기능 추가

--- src/main/java/com/example/toodle_server_springboot/controller/UserAccountController.java
+++ src/main/java/com/example/toodle_server_springboot/controller/UserAccountController.java
@@ -8,11 +8,15 @@ import com.example.toodle_server_springboot.dto.request.UserAccountRegisterReque
 import com.example.toodle_server_springboot.dto.response.UserAccountAuthenticateResponse;
 import com.example.toodle_server_springboot.dto.response.UserAccountResponse;
 import com.example.toodle_server_springboot.service.UserAccountService;
+import com.sun.istack.NotNull;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpSession;
 
 @RestController
 @RequestMapping("/api/v1/users")
@@ -24,8 +28,12 @@ public class UserAccountController {
     private final JwtUserDetailsService jwtUserDetailsService;
     private final AuthenticationManager authenticationManager;
 
+    @Value("${spring.session.redis.namespace}")
+    private String redisSessionNamespace;
+
     /**
      * 사용자 회원 가입 호출 컨트롤러
+     *
      * @param userAccountRegisterRequest
      * @return
      */
@@ -39,15 +47,21 @@ public class UserAccountController {
 
     /**
      * 사용자가 email 과 password 를 보낸 경우, 인증된 사용자라면 적절한 토큰을 발급한다.
+     *
      * @param request
      * @return
      * @throws Exception
      */
     @RequestMapping(value = "/login", method = RequestMethod.POST)
-    public ResponseEntity<?> authenticateUser(@RequestBody UserAccountAuthenticateRequest request) throws Exception{
+    public ResponseEntity<?> authenticateUser(
+            @RequestBody UserAccountAuthenticateRequest request,
+            @NotNull HttpSession session
+    ) throws Exception {
         var authenticationToken = new UsernamePasswordAuthenticationToken(request.email(), request.password());
         authenticationManager.authenticate(authenticationToken);
         var userDetails = jwtUserDetailsService.loadUserByUsername(request.email());
+        session.setAttribute(redisSessionNamespace, request.email());
+        String value = (String) session.getAttribute(redisSessionNamespace);
         var token = jwtTokenUtil.generateToken(userDetails);
         return ResponseEntity.ok(new UserAccountAuthenticateResponse(token));
     }
