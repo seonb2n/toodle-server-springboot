3b85928515bca66bcea9a8a3e5ad398ca6b2400a
seonb2n <626ksb@gmail.com>
2023-05-15 21:25:00 +0900

    [CORS]  - SecurityConfig 에서 cors 설정을 default 로 함  - CorsController 에서 다양한 경우에 대해서 cors 테스트를 함  - @CrossOrigin 설정을 하지 않은 경우 에러 발생 확인

--- src/main/java/com/example/toodle_server_springboot/config/security/SecurityConfig.java
+++ src/main/java/com/example/toodle_server_springboot/config/security/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.example.toodle_server_springboot.config.security;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,11 +12,13 @@ import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.web.cors.CorsConfiguration;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    @Value("${local.react.ip}")
+    String REACT_IP;
 
     @Bean
     public SecurityFilterChain securityFilterChain(
@@ -25,10 +28,11 @@ public class SecurityConfig {
         return http.authorizeHttpRequests(auth -> auth
                         .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
                         .antMatchers("/api/v1/users/**").permitAll()
+                        .antMatchers("/cors/**").permitAll()
                         .anyRequest().authenticated()
                 )
-                .cors().configurationSource(request -> new CorsConfiguration().applyPermitDefaultValues())
-                .and()
+//                .cors().configurationSource(corsConfigurationSource())
+//                .and()
                 .exceptionHandling()
                 .authenticationEntryPoint(jwtAuthenticationEntryPoint)
                 .and()
@@ -36,6 +40,19 @@ public class SecurityConfig {
                 .addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
+
+//    @Bean
+//    public CorsConfigurationSource corsConfigurationSource() {
+//
+//        CorsConfiguration configuration = new CorsConfiguration();
+//        configuration.addAllowedOrigin(REACT_IP); // 허용할 특정 오리진
+//        configuration.addAllowedMethod("*"); // 모든 HTTP 메서드 허용
+//        configuration.addAllowedHeader("*"); // 모든 헤더 허용
+//
+//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//        source.registerCorsConfiguration("/**", configuration);
+//        return source;
+//    }
 
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
