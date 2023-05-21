9bb135a72ef7184ae997631aad2c6cebeb15e0f8
seonb2n <626ksb@gmail.com>
2023-05-21 13:07:21 +0900

    [FEATURE]  - 네이버 아이디 로그인 구현을 위한 security 설정 추가

--- src/main/java/com/example/toodle_server_springboot/config/security/SecurityConfig.java
+++ src/main/java/com/example/toodle_server_springboot/config/security/SecurityConfig.java
@@ -1,5 +1,8 @@
 package com.example.toodle_server_springboot.config.security;
 
+import com.example.toodle_server_springboot.dto.security.NaverOauth2Response;
+import com.example.toodle_server_springboot.dto.security.UserPrincipal;
+import com.example.toodle_server_springboot.service.UserAccountService;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -7,8 +10,14 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
@@ -21,15 +30,18 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(
             HttpSecurity http,
             JwtRequestFilter jwtRequestFilter,
-            JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint) throws Exception {
+            JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint,
+            OAuth2UserService<OAuth2UserRequest, OAuth2User> oAuth2UserService
+            ) throws Exception {
         return http.authorizeHttpRequests(auth -> auth
                         .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
                         .antMatchers("/api/v1/users/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 // 간편 로그인 기능 사용을 위한 설정
-//                .oauth2Login()
-//                .and()
+                .oauth2Login(oAuth -> oAuth
+                        .userInfoEndpoint(userInfo -> userInfo.userService(oAuth2UserService))
+                )
                 .cors().configurationSource(request -> new CorsConfiguration().applyPermitDefaultValues())
                 .and()
                 .exceptionHandling()
@@ -48,6 +60,50 @@ public class SecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    /**
+     * 인증 정보를 가져오는 userDetailsService 를 생성
+     *
+     * @return
+     */
+    @Bean
+    public UserDetailsService userDetailsService(UserAccountService userAccountService) {
+        return username -> userAccountService
+                .findUserAccountDto(username)
+                .map(UserPrincipal::from)
+                //해당되는 사용자가 없는 경우 Exception 처리
+                .orElseThrow(() -> new UsernameNotFoundException("유저를 찾을 수 없습니다. - username: " + username));
+    }
+
+    /**
+     * OAuth2 에서 사용될 userService
+     * OAuth2 로부터 가져온 정보를 바탕으로 우리 db 에 사용자 등록을 한다.
+     * @param userAccountService
+     * @param passwordEncoder
+     * @return
+     */
+    @Bean
+    public OAuth2UserService<OAuth2UserRequest, OAuth2User> oAuth2UserService(
+            UserAccountService userAccountService,
+            PasswordEncoder passwordEncoder
+    ) {
+        final DefaultOAuth2UserService delegate = new DefaultOAuth2UserService();
+        return userRequest -> {
+            OAuth2User oAuth2User = delegate.loadUser(userRequest);
+            NaverOauth2Response naverOauth2Response = NaverOauth2Response.from(oAuth2User.getAttributes());
+            String userEmail = "NAVER_" + naverOauth2Response.email();
+            String dummyPassword = passwordEncoder.encode("{bcrypt}dummy");
+
+            return userAccountService.findUserAccountDto(userEmail)
+                    .map(UserPrincipal::from)
+                    .orElseGet(() ->
+                            UserPrincipal.from(userAccountService.registerUser(
+                                    userEmail,
+                                    dummyPassword,
+                                    naverOauth2Response.nickname()
+                            )));
+        };
     }
 }
 
